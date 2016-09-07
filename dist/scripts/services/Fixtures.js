cfe403e4f722557c19295189e51c29288cd66bff
Ryan Kirkland <ryankirkland@Ryans-MacBook-Air.local>
2016-09-06 19:15:39 -0700

    Assignment: Completed

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -1,15 +1,13 @@
 (function() {
 	function Fixtures() {
 		var Fixtures = {};
-		return Fixtures;
-	}
 
 	var albumPicasso = {
     title: 'The Colors',
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: '/assets/images/album_covers/01.png',
+    albumArtUrl: '../assets/images/album_covers/01.png',
     songs: [
         {title: 'Blue', duration: '161.71', audioUrl: 'assets/music/blue'},
         {title: 'Green', duration: '103.96', audioUrl: 'assets/music/green'},
@@ -24,7 +22,7 @@
      artist: 'Guglielmo Marconi',
      label: 'EM',
      year: '1909',
-     albumArtUrl: '/assets/images/album_covers/20.png',
+     albumArtUrl: '../assets/images/album_covers/20.png',
      songs: [
          { title: 'Hello, Operator?', duration: '1:01' },
          { title: 'Ring, ring, ring', duration: '5:01' },
@@ -37,6 +35,20 @@
  	Fixtures.getAlbum = function() {
  		return albumPicasso;
  	};
+
+ 	Fixtures.getCollection = function(numberOfAlbums) {
+ 		
+ 		var albumCollection = [];
+
+ 		for (var i = 0; i < numberOfAlbums; i++) {
+ 			albumCollection.push(angular.copy(albumPicasso));
+ 		}
+
+ 		return albumCollection;
+ 	};
+
+ return Fixtures;
+}
 
 	angular
 		.module('blocJams')
